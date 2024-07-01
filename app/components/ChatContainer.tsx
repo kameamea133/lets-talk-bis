6f529af5f7f6ccd5660ba39993a99d03bfe8c42f
kameamea133 <monnier1977@gmail.com>
2024-07-01 10:11:49 +0200

    feat: Add image upload functionality with preview and removal option
    - Implemented image upload feature in the message input component.
    - Added the ability to preview selected images before sending.
    - Included a button to remove the selected image before sending.
    - Updated UI to enhance user experience with image upload.

--- app/components/ChatContainer.tsx
+++ app/components/ChatContainer.tsx
@@ -9,10 +9,49 @@ interface Message {
     name: string; 
     text: string; 
     avatar:string;
+    imageUrl?: string;
     createdAt: string;
 }
 export default function ChatContainer() {
+
+    const [data, setData] = useState<Message[]>([]);
+    const {user} = useClientAuth();
+
+    useEffect(() => {
+        const q = query(collection(db, 'messages'), orderBy('createdAt'), limit(50));
+        const unsubscribe = onSnapshot(q, (querySnapshot) => {
+            let messages: Message[] = [];
+            querySnapshot.forEach((doc) => {
+                const data = doc.data();
+                messages.push({
+                    id: data.id,
+                    name: data.name,
+                    text: data.text,
+                    avatar: data.avatar,
+                    imageUrl: data.imageUrl,
+                    createdAt: data.createdAt ? data.createdAt.toDate() : new Date(),
+                });
+            });
+            setData(messages);
+        })
+        return () => unsubscribe()
+    }, [])
+
   return (
-    <div>ChatContainer</div>
+    <div className='max-w-[700px] h-screen m-auto'>
+        <ul className='flex flex-col gap-3 p-10 pb-[150px]'>
+            {data.map((msg, index) => (
+                <li key={index} className={`relative flex items-center p-3 max-w-xs rounded-lg  ${msg.id === user?.uid ? 'self-end rounded-br-none bg-blue-300 text-blue-900' : 'self-start rounded-bl-none bg-gray-300 text-gray-900'}`}>
+                    <Image src={msg.avatar} alt="user icon" width={50} height={50} className='rounded-full mr-3'/>
+                    <div>
+                        <p className='font-bold'>{msg.name}</p>
+                        <p>{msg.text}</p>
+                        {msg.imageUrl && <img src={msg.imageUrl} alt="uploaded" className='mt-2 rounded' style={{ maxWidth: '100px' }} />}
+                        <span className='text-xs text-gray-600'>{msg.createdAt.toLocaleString()}</span>
+                    </div>
+                </li>
+            ))}
+        </ul>
+    </div>
   )
 }
--- app/components/SendMessage.tsx
+++ app/components/SendMessage.tsx
@@ -4,53 +4,114 @@
 
 import React, { useState, ChangeEvent, FormEvent } from 'react'
 import useClientAuth from '../hooks/useClientAuth'
-import {db} from '../db/firebaseConfig'
+import {db, storage} from '../db/firebaseConfig'
 import { collection, addDoc, serverTimestamp } from "firebase/firestore";
+import { BsFileImage } from "react-icons/bs";
+import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
 import { IoSend } from 'react-icons/io5'
+import { MdCancel } from "react-icons/md";
+
 
 export default function SendMessage() {
     const [value, setValue] = useState('')
+    const [image, setImage] = useState<File | null>(null);
+    const [imagePreview, setImagePreview] = useState<string | null>(null);
     const {user} = useClientAuth()
 
+
+    const handleImageChange = (e: ChangeEvent<HTMLInputElement>) => {
+        if (e.target.files && e.target.files[0]) {
+            const file = e.target.files[0];
+            setImage(e.target.files[0]);
+            setImagePreview(URL.createObjectURL(file));
+        }
+    };
 
     const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
         const val = e.target.value;
         setValue(val)
     }
 
-    const handleSubmit = async(e: FormEvent) => {
+    const handleRemoveImage = () => {
+        setImage(null);
+        setImagePreview(null);
+    };
+
+
+    const handleSubmit = async (e: FormEvent) => {
         e.preventDefault();
 
-        try{
-            if(user) {
-                const {uid, displayName, photoURL} = user;
+        try {
+            if (user) {
+                const { uid, displayName, photoURL } = user;
+                let imageUrl = '';
+
+                if (image) {
+                    const imageRef = ref(storage, `images/${image.name}`);
+                    const snapshot = await uploadBytes(imageRef, image);
+                    imageUrl = await getDownloadURL(snapshot.ref);
+                }
+
                 await addDoc(collection(db, 'messages'), {
                     text: value,
                     id: uid,
                     name: displayName,
                     avatar: photoURL,
+                    imageUrl: imageUrl,
                     createdAt: serverTimestamp()
-                })
+                });
             }
+        } catch (err) {
+            console.log(err);
+        }
+        setValue('');
+        setImage(null);
+        setImagePreview(null);
+    };
 
-        }catch(err){
-            console.log(err)
-    }
-    setValue('')
-}
+    //     const handleSubmit = async(e: FormEvent) => {
+//         e.preventDefault();
+
+//         try{
+//             if(user) {
+//                 const {uid, displayName, photoURL} = user;
+//                 await addDoc(collection(db, 'messages'), {
+//                     text: value,
+//                     id: uid,
+//                     name: displayName,
+//                     avatar: photoURL,
+//                     createdAt: serverTimestamp()
+//                 })
+//             }
+
+//         }catch(err){
+//             console.log(err)
+//     }
+//     setValue('')
+// }
 
 
   return (
     <form onSubmit={handleSubmit} className='bg-blue-200 fixed bottom-0 w-full py-10 flex items-center justify-center px-3 flex-col'>
-
-
     <div className='w-full flex items-center justify-center px-3'>
-        <input value={value} onChange={handleChange} placeholder='Votre message...' type="text" className='p-3 w-full outline-none border-none rounded-l-md'/>
+        <input type="file" onChange={handleImageChange} className='hidden' id='imageUpload'/>
+        <label htmlFor='imageUpload' className='cursor-pointer p-3 bg-gray-300 rounded-l-md'>
+            <BsFileImage size={24} />
+        </label>
+        <input value={value} onChange={handleChange} placeholder='Votre message...' type="text" className='p-3 w-full outline-none border-none'/>
         <button type="submit" className="bg-green-500 text-white p-3 flex items-center gap-2 border-none rounded-r-md">
             <IoSend />
             <span>Envoyer</span>
         </button>
     </div>
-    </form>
+    {imagePreview && (
+                <div className='mt-2 flex items-center'>
+                    <img src={imagePreview} alt="Prévisualisation" className='rounded' style={{ maxWidth: '200px' }} />
+                    <button onClick={handleRemoveImage} className='ml-2 text-red-500'>
+                        <MdCancel size={24} />
+                    </button>
+                </div>
+            )}
+</form>
   )
 }
